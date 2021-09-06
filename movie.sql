1cd2eebbb199a7e0e4181e299f2280da68aecc33
Chunjie Nan <60551793+nancunjie4560@users.noreply.github.com>
2021-09-05 21:15:51 -0400

    Add files via upload

---
+++ movie.sql
@@ -0,0 +1,19 @@
+-- drop table movies;
+create table movies (
+ 	respondent varchar(50),
+ 	still_water int,
+	F9 int,
+	shang_chi int,
+	dont_breath_2 int,
+	black_widow int,
+	rogue_hostage int
+)
+;
+
+insert into movies (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Misun', 5,4,2,4,2,NULL);
+insert into movies (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Juyong', 4,NULL,3,5,3,5);
+insert into movies (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Gloria', NULL,3,3,NULL,1,4);
+insert into movies (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Kevin', 4,4,NULL,NULL,3,4);
+insert into movies (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('John', 4,3,1,3,2,4);
+
+-- select * from movies;
