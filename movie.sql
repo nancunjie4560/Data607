358dfc71e8ace30b4d0e08db73d9128f738db2df
Chunjie Nan <60551793+nancunjie4560@users.noreply.github.com>
2021-09-05 21:50:35 -0400

    Update movie.sql

--- movie.sql
+++ movie.sql
@@ -1,25 +1,22 @@
 --drop table movies;
 create table movies (
  	respondent varchar(50),
- 	still_water int,
-	F_9 int,
+ 	F_F_Nine int,
+	still_water int,
 	shang_chi int,
-	dont_breath_2 int,
+	dont_breath_Two int,
 	black_widow int,
 	rogue_hostage int
 )
 ;
 
-insert into movies (respondent, still_water, F_9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Misun', 5,4,2,4,2,NULL);
-insert into movies (respondent, still_water, F_9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Juyong', 4,NULL,3,5,3,5);
-insert into movies (respondent, still_water, F_9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Gloria', NULL,3,3,NULL,1,4);
-insert into movies (respondent, still_water, F_9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Kevin', 4,4,NULL,NULL,3,4);
-insert into movies (respondent, still_water, F_9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('John', 4,3,1,3,2,4);
-
-
+insert into movies (respondent, F_F_Nine, still_water, shang_chi, dont_breath_Two, black_widow, rogue_hostage) values ('Misun', 5,4,3,NULL,2,NULL);
+insert into movies (respondent, F_F_Nine, still_water, shang_chi, dont_breath_Two, black_widow, rogue_hostage) values ('John', 4,NULL,2,5,3,5);
+insert into movies (respondent, F_F_Nine, still_water, shang_chi, dont_breath_Two, black_widow, rogue_hostage) values ('Juyong', 4,NULL,1,NULL,3,4);
+insert into movies (respondent, F_F_Nine, still_water, shang_chi, dont_breath_Two, black_widow, rogue_hostage) values ('Kevin', 4,3,NULL,5,4,4);
+insert into movies (respondent, F_F_Nine, still_water, shang_chi, dont_breath_Two, black_widow, rogue_hostage) values ('Juju', 5,4,3,4,4,NULL);
 
 --select * from movies;
-
 
 
 
