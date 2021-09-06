dc078d7c0595af00bcee52561a6c7703a42c0351
Chunjie Nan <60551793+nancunjie4560@users.noreply.github.com>
2021-09-05 21:25:50 -0400

    Update movie.sql

--- movie.sql
+++ movie.sql
@@ -14,6 +14,6 @@ insert into movie (respondent, still_water, F9, shang_chi, dont_breath_2, black_
 insert into movie (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Juyong', 4,NULL,3,5,3,5);
 insert into movie (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Gloria', NULL,3,3,NULL,1,4);
 insert into movie (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('Kevin', 4,4,NULL,NULL,3,4);
-insert into movies(respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('John', 4,3,1,3,2,4);
+insert into movie (respondent, still_water, F9, shang_chi, dont_breath_2, black_widow, rogue_hostage) values ('John', 4,3,1,3,2,4);
 
 --select * from movie;
